ad4b4d6f63db76a24d2adc72504318ca5652f3c6
Kasper Pylkkänen <dkapy003@edu.xamk.fi>
2023-11-15 10:31:20 +0200

    Moved scripts and made singleton

---
+++ GameProgrammingPatterns/Assets/Scripts/Singleton/AudioManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioManager Instance { get; private set; }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
